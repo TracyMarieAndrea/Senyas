f4ece416b5310785a3f9e1e4b8fe637a1d4e3983
TracyMarieAndrea <tracymarieandrea.madresta@wvsu.edu.ph>
2023-05-29 11:12:13 +0800

    first commit

---
+++ lib/view/home.dart
@@ -0,0 +1,26 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:senyas/main.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.removeAfter(initialization);
+  runApp(const MyApp());
+}
+
+Future initialization(BuildContext context) async {
+  await Future.delayed(const Duration(seconds: 3));
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/view/instruction.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class InstructionScreen extends StatelessWidget {
+  const InstructionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
