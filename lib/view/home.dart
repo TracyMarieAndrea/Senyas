b32b03e69e0dfdbd2838d6a5202337d00c1824db
TracyMarieAndrea <tracymarieandrea.madresta@wvsu.edu.ph>
2023-06-04 12:01:30 +0800

    edit -tracy

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:senyas/main.dart';
-
-import 'drawer.dart';
+import 'package:tflite/tflite.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,16 +14,95 @@ Future initialization(BuildContext context) async {
   await Future.delayed(const Duration(seconds: 3));
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  CameraImage? cameraImage;
+  CameraController? cameraController;
+  String output = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadCamera();
+    loadmodel();
+  }
+
+  loadCamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.high);
+    cameraController!.initialize().then((value) {
+      if (!mounted) {
+        return;
+      } else {
+        setState(() {
+          cameraController!.startImageStream((imageStream) {
+            cameraImage = imageStream;
+            runModel();
+          });
+        });
+      }
+    });
+  }
+
+  runModel() async {
+    if (cameraImage != null) {
+      var predictions = await Tflite.runModelOnFrame(
+        bytesList: cameraImage!.planes.map((plane) {
+          return plane.bytes;
+        }).toList(),
+        imageHeight: cameraImage!.height,
+        imageWidth: cameraImage!.width,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        rotation: 90,
+        numResults: 2,
+        threshold: 0.1,
+        asynch: true,
+      );
+      predictions!.forEach((element) {
+        setState(() {
+          output = element['label'];
+        });
+      });
+    }
+  }
+
+  loadmodel() async {
+    await Tflite.loadModel(
+      model: 'assets/model.tflite',
+      labels: 'assets/labels2.txt',
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
-      ),
-      drawer: AppDrawer(),
+      appBar: AppBar(title: Text('Live Emotion Detection App')),
+      body: Column(children: [
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            width: MediaQuery.of(context).size.width,
+            child: !cameraController!.value.isInitialized
+                ? Container()
+                : AspectRatio(
+                    aspectRatio: cameraController!.value.aspectRatio,
+                    child: CameraPreview(cameraController!),
+                  ),
+          ),
+        ),
+        Text(
+          output,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        )
+      ]),
     );
   }
 }
